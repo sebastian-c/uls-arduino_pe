616b62383f330ddb7f03e236d2d7f071273be238
Sebastian Campbell <sebastian-c@users.noreply.github.com>
2023-03-10 16:33:10 +0100

    web server now accepts today's temp

--- api_server.py
+++ api_server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import time
+from datetime import datetime
 
 exec(open("temperature_average.py").read())
 
@@ -16,3 +18,18 @@ def show_sensor_location(sensor_location):
     except KeyError:
         return f'Could not find sensor location: {sensor_location}'
     return f'Sensor_location: {sensor_location}<br>Average_temperature: {all_temps[str.lower(sensor_location)]}'
+
+@app.route('/<sensor_location>/today')
+def show_today_temp(sensor_location):
+    
+    try:
+        sensor_temperature = all_temps[str.lower(sensor_location)]
+    except KeyError:
+        return f'Could not find sensor location: {sensor_location}'
+    
+    is_today = all_data.Timestamp.apply(datetime.fromtimestamp) > datetime.combine(datetime.now(), datetime.min.time())
+    sensor_data  = all_data.loc[is_today].groupby("location_x").Temperature.mean()
+    
+    return f'Sensor_location: {sensor_location}<br>Average_temperature today: {sensor_data[str.lower(sensor_location)]}'
+
+
