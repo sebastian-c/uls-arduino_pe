92bfd336876470c555e8410c85ceacbd43e759d9
Sebastian Campbell <sebastian-c@users.noreply.github.com>
2023-03-10 11:36:47 +0100

    Removed extraneous comments from arduino

--- weather_upload/weather_upload.ino
+++ weather_upload/weather_upload.ino
@@ -8,26 +8,11 @@
 #include "DHT.h"
 
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
 
-// Uncomment whatever type you're using!
+// Define model
 #define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
-// Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
 // Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
@@ -69,4 +54,5 @@ void loop() {
   Serial.println(F("}"));
 
   //if() for checking button press
+  // Serial.println(F("[COMMAND] Button press registered on pin 7"))
 }
